a7e097372cb91063fca7d033ba99583529740279
Arindam Sarkar <asarkar1990@gmail.com>
2021-03-20 13:42:55 +0530

    20/03/2021

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -4,6 +4,7 @@ import 'package:flutter_free_commerce/models/models.dart';
 import 'package:flutter_free_commerce/services/product_service.dart';
 
 
+
 // View <===> Controller <===> Service
 
 class ProductController {
