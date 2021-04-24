e31733c8ab3af284dde4fe98dbd6515d5e41c56d
Arindam Sarkar <asarkar1990@gmail.com>
2021-04-24 14:37:57 +0530

    24/04/2021

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_free_commerce/controllers/cart_controller.dart';
 import 'package:flutter_free_commerce/views/auth_view.dart';
-import 'package:flutter_free_commerce/views/home_view.dart';
 
 
 
